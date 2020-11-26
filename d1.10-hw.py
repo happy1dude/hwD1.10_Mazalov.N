bbd8fe1b8aaeb245f8375d09671bd65c36cd66ba
happy1dude <63054916+happy1dude@users.noreply.github.com>
2020-11-26 20:13:43 +0700

    Update d1.10-hw.py

--- d1.10-hw.py
+++ d1.10-hw.py
@@ -16,6 +16,19 @@ trelloNew =  TrelloApi(auth_params['key'], auth_params['token'])
 base_url = "https://api.trello.com/1/{}" 
 board_id = "TOLfDH0N"
 
+def get_task_duplicates(task_name):  
+    # Получим данные всех колонок на доске  
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()  
+  
+    # Заведём список колонок с дублирующимися именами  
+    duplicate_tasks = []  
+    for column in column_data:  
+        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()  
+        for task in column_tasks:  
+            if task['name'] == task_name:  
+                duplicate_tasks.append(task)  
+    return duplicate_tasks
+
 def newBoard(name):
     #Создаём новую доску
     newboard = trelloNew.boards.new(name)
@@ -35,7 +48,7 @@ def read():
             print('\t' + 'No have tasks!')
             continue
         for task in task_data:
-            print('\t' + task['name'])
+            print('\t' + task['name'] + '\t' + task['id'])
 
 def createList(name):
     #Получим данные о нужной доске
@@ -69,28 +82,45 @@ def createNewTask(name, column_name):
 
 
 def move(name, column_name):
+    duplicate_tasks = get_task_duplicates(name)
+    if len(duplicate_tasks) > 1:
+        print("Задач с таким названием несколько штук:")
+        for index, task in enumerate(duplicate_tasks):
+            task_column_name = requests.get(base_url.format('lists') + '/' + task['idList'], params=auth_params).json()['name']
+            print("Задача №{}\tid: {}\tНаходится в колонке: {}\t ".format(index, task['id'], task_column_name))
+        task_id = input("Пожалуйста, введите ID задачи, которую нужно переместить: ")
+    else:
+        task_id = duplicate_tasks[0]['id']
+
+    # Теперь, у нас есть id задачи, которую мы хотим переместить. Получим ID колонки, в которую мы будем перемещать задачу
+    column_id = column_check(column_name)
+    if column_id is None:
+        column_id = create_column(column_name)['id']
+    
+    requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column_id, **auth_params})
+    
     #Получим данные со всех колонок на доске
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
     # print(column_data)
 
-    #Среди всех колонок нужно найти задачу по имени и получить её id
-    task_id = None
-    for column in column_data:
-        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
-        for task in column_tasks:
-            if task['name'] == name:
-                task_id = task['id']
-                break
-        if task_id:
-            break
+#     #Среди всех колонок нужно найти задачу по имени и получить её id
+#     task_id = None
+#     for column in column_data:
+#         column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+#         for task in column_tasks:
+#             if task['name'] == name:
+#                 task_id = task['id']
+#                 break
+#         if task_id:
+#             break
 
-    #Теперь у нас есть id задачи, которую мы хотим переместить
-    #Переберем данные обо всех колонках, пока не найдём ту, в которую мы будем перемещать задачу
-    for column in column_data:
-        if column['name'] == column_name:
-            #И выполним запрос к API для перемещения задачи в нужную колонку
-            requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})
-            break
+#     #Теперь у нас есть id задачи, которую мы хотим переместить
+#     #Переберем данные обо всех колонках, пока не найдём ту, в которую мы будем перемещать задачу
+#     for column in column_data:
+#         if column['name'] == column_name:
+#             #И выполним запрос к API для перемещения задачи в нужную колонку
+#             requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})
+#             break
 
 
 if __name__ == "__main__":
